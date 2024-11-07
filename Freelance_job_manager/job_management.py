b2b5ee4423c04e22efe3f3b472fd25cc3714c1b9
Lightstats <hirwa12ariane@gmail.com>
2024-11-07 14:55:20 +0200

    Initializing data structures

--- Freelance_job_manager/job_management.py
+++ Freelance_job_manager/job_management.py
@@ -1,2 +1,10 @@
+clients = {}
+job_id_counter = 0
+
 def add_job():
    """Add a new job"""
+   global job_id_counter
+
+   if not clients:
+       print("\nNo clients available. Please add a client first.")
+       return
