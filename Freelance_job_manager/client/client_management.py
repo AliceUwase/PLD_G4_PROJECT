1e736a398de6ee3440ecb67d2ef195962c4fe03d
Iradukunda Christian Tonny <spcsgaba@gmail.com>
2024-11-07 21:18:45 +0200

    Added the client management class and function to declare and initalize the id counter

--- Freelance_job_manager/client/client_management.py
+++ Freelance_job_manager/client/client_management.py
@@ -1,16 +1,4 @@
-def add_client():
-    """Add a new client to the system"""
-    global client_id_counter
-    print("\n-- Add New Client --")
-    name = input("Enter client name: ")
-    contact = input("Enter client contact (email/phone): ")
-    notes = input("Enter any notes (optional): ")
-
-    clients[client_id_counter] = {
-        'name': name,
-        'contact': contact,
-        'notes': notes
-    }
-    print(f"\nClient added successfully! Client ID: {client_id_counter}")
-    client_id_counter += 1
-
+class ClientManagement:
+    def __init__(self):
+        self.client = {}
+        self.client_id_counter = 1
