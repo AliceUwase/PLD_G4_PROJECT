9fafc740a093ee96f2012ca388026a99d0d40c96
Larissa4-droid <l.iriza@alustudent.com>
2024-11-14 14:52:46 +0200

    Update client_management.py

--- Freelance_job_manager/client/client_management.py
+++ Freelance_job_manager/client/client_management.py
@@ -4,3 +4,4 @@ class ClientManager:
     def add_client(self,name,contact_info):    
         client = {"name":name,"contact_info": contact_info}
         self.clients.append(client)
+        print(f"client'{name}'added.")
